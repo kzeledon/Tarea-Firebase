0115837fddd925d87a79eb14b10d5ccdb6b845be
Karina <karizp14@gmail.com>
2018-05-24 20:14:35 -0600

    Layout Creados

---
+++ app/src/main/java/com/example/karina/firebase/ItemsActivity.java
@@ -0,0 +1,13 @@
+package com.example.karina.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items);
+    }
+}
